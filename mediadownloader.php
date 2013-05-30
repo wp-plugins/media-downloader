25111219b53ccdea44ec0d37d2dcb0bfd0ed5448
edersonpeka <edersonpeka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-30 16:51:17 +0000

    Fixing a little mess in the sorting logic (I changed the array keys and forgot to fix it elsewhere).
    git-svn-id: https://plugins.svn.wordpress.org/media-downloader/trunk@720475 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mediadownloader.php
+++ mediadownloader.php
@@ -3,7 +3,7 @@
 Plugin Name: Media Downloader
 Plugin URI: http://ederson.peka.nom.br
 Description: Media Downloader plugin lists MP3 files from a folder by replacing the [media] smarttag.
-Version: 0.1.99.86
+Version: 0.1.99.87
 Author: Ederson Peka
 Author URI: http://ederson.peka.nom.br
 */
@@ -366,7 +366,7 @@ function listMedia( $t ){
                     $alltags[$ifile] = $ftags;
                     // Populating array of tag values with all tags
                     foreach ( $mdtags as $mshowtag )
-                        $tagvalues[$mshowtag][$ifile] = ( 'comment' == $mshowtag ) ? Markdown( $ftags[$mshowtag][0] ) : $ftags[$mshowtag][0];
+                        $tagvalues[$mshowtag][$ifile.'.'.$iext] = ( 'comment' == $mshowtag ) ? Markdown( $ftags[$mshowtag][0] ) : $ftags[$mshowtag][0];
                 }
                 // Calculating tag "prefixes"
                 $tagprefixes = array();
@@ -391,7 +391,7 @@ function listMedia( $t ){
                     $ititle = '';
                     // Each tag list item
                     foreach ( $mshowtags as $mshowtag ) {
-                        $tagvalue = $tagvalues[$mshowtag][$ifile];
+                        $tagvalue = $tagvalues[$mshowtag][$ifile.'.'.$iext];
                         if ( '' == $tagvalue ) {
                             $tagvalue = '&nbsp;';
                         } else {
