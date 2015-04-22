2d70f6f9fc076ad238c7ae1762d5aaf9e42d9d91
edersonpeka <edersonpeka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 01:32:41 +0000

    Removing unnecessary printing of variable without sanitization.
    git-svn-id: https://plugins.svn.wordpress.org/media-downloader/trunk@1141562 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- getfile.php
+++ getfile.php
@@ -22,7 +22,7 @@ function dl_file_resumable( $file, $is_resume = TRUE )
     //First, check if the file exists
     if ( !is_file($file) && ( $file = stripslashes( $file ) ) && !is_file($file) )
     {
-        die("<b>404 File not found! <!-- Searched for file: \"" . htmlentities( $file ) . "\" --></b>");
+        die( '<b>' . _md( 'Error 404: File not found!' ) . '</b>' );
     }
 
     //Gather relevent info about file
