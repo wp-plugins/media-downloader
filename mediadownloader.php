00263b8e05fe972ac280e144b0bd760827408d2d
edersonpeka <edersonpeka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 00:27:38 +0000

    Updating compatibility info
    git-svn-id: https://plugins.svn.wordpress.org/media-downloader/trunk@1141530 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mediadownloader.php
+++ mediadownloader.php
@@ -3,7 +3,7 @@
 Plugin Name: Media Downloader
 Plugin URI: http://ederson.peka.nom.br
 Description: Media Downloader plugin lists MP3 files from a folder by replacing the [media] smarttag.
-Version: 0.1.993
+Version: 0.2
 Author: Ederson Peka
 Author URI: http://ederson.peka.nom.br
 Text Domain: media-downloader
